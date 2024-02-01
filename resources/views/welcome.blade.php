e67072b0f480c1ad169706d8129ccd8e5187efe5
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2024-02-01 23:59:36 +0100

    Migrate

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,13 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
